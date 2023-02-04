fabb127bcaf85ec3deda9d3907f604ec18975f61
Melissa Aime <aimemelissa1@gmail.com>
2023-02-04 11:05:33 -0300

    first commit - struture

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Styles/Styles.scss";
+import "./App.scss";
+import { BrowserRouter, Routes, Route , Navigate } from 'react-router-dom';
+import { Navbar } from './Sections/Navbar/Navbar';
+import { Home } from './Sections/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter> 
+        
+    <Navbar/>
+
+    <Routes>
+        <Route path='/' element={ <Home/> } />
+        {/* <Route path='/profile' element={ <MyProfile/> } />
+        <Route path='/resume' element={ <Resume/> } />
+        <Route path='/proyects' element={ <Proyects/> } />
+        <Route path='/skills' element={ <Skills/> } />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='*' element={ <Navigate to='/' /> } /> */}
+    </Routes>
+      
+  </BrowserRouter> 
   );
 }
 
